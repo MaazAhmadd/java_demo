41de09e717b61debc8c136f85d300434affaab63
maazahmadd <maaz.ahmad213@gmail.com>
2023-12-11 15:43:45 +0500

    linked lists

--- dsa/src/App.java
+++ dsa/src/App.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.reverse();
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+
     }
 }
